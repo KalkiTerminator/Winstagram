10d23ad573aa1208973039f5490a6abae853590c
Prateek_AIProduct <152714339+KalkiTerminator@users.noreply.github.com>
2026-01-25 21:58:55 +0530

    Implement database models and API endpoints for post management

---
+++ app/db.py
@@ -0,0 +1,34 @@
+from typing import AsyncGenerator
+import uuid
+
+from sqlalchemy import Column, String , Text, DateTime, ForeignKey
+from sqlalchemy import UUID
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
+from sqlalchemy.orm import declarative_base, relationship
+from datetime import datetime
+
+
+DATABASE_URL = "sqlite+aiosqlite:///./test.db"
+
+Base = declarative_base()
+
+class Post(Base):
+    __tablename__ = "Posts"
+
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    caption = Column(Text)
+    url = Column(String, nullable=False)
+    file_type = Column(String, nullable=False)
+    file_name = Column(String, nullable=False)
+    created_at = Column(DateTime, default=datetime.utcnow)
+engine = create_async_engine(DATABASE_URL)
+async_session = async_sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+
+async def create_db_and_tables():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+
+async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
+    async with async_session() as session:
+        yield session
+    
--- app/schemas.py
+++ app/schemas.py
@@ -1,9 +1,15 @@
 from pydantic import BaseModel
+from datetime import datetime
+from uuid import UUID
 
-class PostCreate (BaseModel):
-    title: str
-    content: str
+class PostCreate(BaseModel):
+    caption: str
 
-class PostResponse (BaseModel):
-    title: str
-    content: str
+class PostResponse(BaseModel):
+    id: UUID
+    caption: str
+    url: str
+    created_at: datetime
+
+    class Config:
+        from_attributes = True
