ae3c3a40c4864e03d1760c46f2ad51ffddb0e0d9
Prateek_AIProduct <152714339+KalkiTerminator@users.noreply.github.com>
2026-01-26 00:45:29 +0530

    Add image upload functionality and configure ImageKit integration

--- app/app.py
+++ app/app.py
@@ -4,7 +4,11 @@ from app.db import Post, create_db_and_tables, get_async_session
 from sqlalchemy.ext.asyncio import AsyncSession
 from contextlib import asynccontextmanager
 from sqlalchemy import select
-
+from app.images import imagekit
+import shutil
+import os
+import uuid
+import tempfile
 
 
 @asynccontextmanager
@@ -22,17 +26,42 @@ async def upload_post(
     caption: str = Form(...),
     session: AsyncSession = Depends(get_async_session),
 ):
-    post = Post(
-        caption=caption,
-        url="dummy url",
-        file_type="photo",
-        file_name="dummy file name",
-    )
+    
+    temp_file_path = None
 
-    session.add(post)
-    await session.commit()
-    await session.refresh(post)
-    return post
+    try:
+        with tempfile.NamedTemporaryFile(delete=False, suffix=os.path.splitext(file.filename)[1]) as temp_file:
+            temp_file_path = temp_file.name
+            shutil.copyfileobj(file.file, temp_file)
+        
+        with open(temp_file_path, "rb") as file_to_upload:
+            upload_result = imagekit.files.upload(
+                file=file_to_upload,
+                file_name=file.filename,
+                use_unique_file_name=True,
+                tags=["backend_upload"]
+            )
+
+        post = Post(
+            caption=caption,
+            url=upload_result.url,
+            file_type="video" if file.content_type.startswith("video/") else "image",
+            file_name=upload_result.name
+        )
+
+        session.add(post)
+        await session.commit()
+        await session.refresh(post)
+        return post
+        
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+    finally:
+        if temp_file_path and os.path.exists(temp_file_path):
+            os.unlink(temp_file_path)
+        file.file.close()    
+ 
 
 @app.get("/feed")
 async def get_feed(
---
+++ app/images.py
@@ -0,0 +1,6 @@
+from dotenv import load_dotenv
+from imagekitio import ImageKit
+
+load_dotenv()
+
+imagekit = ImageKit()
